36a0fbf9de541dd30e148c270fde1a59ef51bdc5
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-10 22:00:34 -0400

    completed getOneTag function and testing

--- src/api/tags/tag.repository.ts
+++ src/api/tags/tag.repository.ts
@@ -1,8 +1,37 @@
 import { generateSlug } from '../../common/functions/generate-slug';
+import { InvalidFunctionInputError } from '../../errors/invalid-input.error';
+import { NotFoundError } from '../../errors/not-found.error';
 import { TagDocument, TagModel } from './database/tag.entity';
 import { ICreateTagInput } from './interfaces/create-tag-input.interface';
 
-const getOneTag = () => {};
+const getOneTag = async (input: {
+  id?: string;
+  slug?: string;
+}): Promise<TagDocument> => {
+  const { id, slug } = input;
+
+  if (!id && !slug) {
+    throw new InvalidFunctionInputError();
+  }
+
+  let filterInput = {};
+
+  if (id) {
+    filterInput = { _id: id };
+  }
+
+  if (slug) {
+    filterInput = { ...filterInput, slug };
+  }
+
+  const tag = await TagModel.findOne(filterInput);
+
+  if (!tag) {
+    throw new NotFoundError();
+  }
+
+  return tag;
+};
 
 const getAllTags = () => {};
 
