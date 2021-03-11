056e98c88066d768873c5e3a4a4d926124b826ad
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-10 20:15:41 -0400

    completed implementation and testing of generateSlug function

--- src/common/functions/__test__/generate-slug.test.ts
+++ src/common/functions/__test__/generate-slug.test.ts
@@ -12,4 +12,22 @@ describe('generateSlug', () => {
 
     throw new Error('Test failed');
   });
+
+  it('generates a slug given the correct input', () => {
+    const input = ['some', 'strings', 'to', 'test', 'this', 'function'];
+    const expectedResult = 'some-strings-to-test-this-function';
+
+    const slug = generateSlug(input);
+
+    expect(slug).toEqual(expectedResult);
+  });
+
+  it('generates two different slugs if the true flag is passed as second  argument to generateSlug', () => {
+    const input: string[] = ['a', 'test', 'input'];
+
+    const slug1 = generateSlug(input, true);
+    const slug2 = generateSlug(input, true);
+
+    expect(slug1).not.toEqual(slug2);
+  });
 });
