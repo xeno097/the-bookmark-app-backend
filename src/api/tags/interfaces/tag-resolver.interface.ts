b54e7a666821e2e579c50b9e961c610f160f0566
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-13 16:22:39 -0400

    defined tag mutations interface

--- src/api/tags/interfaces/tag-resolver.interface.ts
+++ src/api/tags/interfaces/tag-resolver.interface.ts
@@ -1,6 +1,8 @@
 import { GqlCustomExecutionContext } from '../../../common/interfaces/graphql-custom-context.interface';
 import { TagDocument } from '../database/tag.entity';
+import { ICreateTagInput } from './create-tag-input.interface';
 import { IGetOneTagInput } from './get-one-tag-input.interface';
+import { IUpdateTagInput } from './update-tag-input.interface';
 
 export interface TagQueries {
   tag: (
@@ -12,6 +14,27 @@ export interface TagQueries {
   tags: (
     parent: any,
     args: any,
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<TagDocument[]>;
+}
+
+export interface TagMutations {
+  createTag: (
+    parent: any,
+    args: { input: ICreateTagInput },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<TagDocument>;
+  updateTag: (
+    parent: any,
+    args: { input: IUpdateTagInput },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<TagDocument[]>;
+  deleteTag: (
+    parent: any,
+    args: { input: IGetOneTagInput },
     context: GqlCustomExecutionContext,
     info: any,
   ) => Promise<TagDocument[]>;
