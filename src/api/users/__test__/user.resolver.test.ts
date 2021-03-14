9c8be85721ce1b0923ff6efb73f273c4e07a8976
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-14 19:13:33 -0400

    started implementing and testing the signUp mutation

---
+++ src/api/users/__test__/user.resolver.test.ts
@@ -0,0 +1,11 @@
+describe('UserResolver', () => {
+  describe('signUp', () => {
+    it('throws an error if the email is already in use', () => {});
+
+    it('throws an error if the username is already in use', () => {});
+
+    it('throws an error if the passwords do not match', () => {});
+
+    it('returns the user data and a jwt token given a valid sign up input', () => {});
+  });
+});
