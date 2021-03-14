38988dbe3122e540e36cc12e6821da763289eced
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-14 16:12:35 -0400

    implemented and tested signUp function

--- src/api/users/__test__/user.repository.test.ts
+++ src/api/users/__test__/user.repository.test.ts
@@ -1,11 +1,90 @@
+import { UserModel } from '../database/user.entity';
+import { ISignUpInput } from '../interfaces/sign-up-input.interface';
+import { signUp } from '../user.repository';
+
 describe('UserRepository', () => {
+  const setup = async () => {
+    const user = UserModel.build({
+      email: 'test@test.com',
+      password: '1234567890',
+      username: 'testuser',
+    });
+
+    await user.save();
+
+    return user;
+  };
+
   describe('signUp', () => {
-    it('throws an error if the email is already in use', async () => {});
+    it('throws an error if the email is already in use', async (done) => {
+      const user = await setup();
 
-    it('throws an error if the username is already is use', async () => {});
+      const input: ISignUpInput = {
+        confirmPassword: '1234567890',
+        password: '1234567890',
+        email: user.email,
+        username: 'user',
+      };
 
-    it('throws an error if the passwords do not match', async () => {});
+      try {
+        await signUp(input);
+      } catch (error) {
+        return done();
+      }
 
-    it('successfully creates a user given a valid input', async () => {});
+      throw new Error('Test failed');
+    });
+
+    it('throws an error if the username is already is use', async (done) => {
+      const user = await setup();
+
+      const input: ISignUpInput = {
+        confirmPassword: '1234567890',
+        password: '1234567890',
+        email: 'email@test.com',
+        username: user.username,
+      };
+
+      try {
+        await signUp(input);
+      } catch (error) {
+        return done();
+      }
+
+      throw new Error('Test failed');
+    });
+
+    it('throws an error if the passwords do not match', async (done) => {
+      const user = await setup();
+
+      const input: ISignUpInput = {
+        confirmPassword: '12345678',
+        password: '1234567890',
+        email: 'email@test.com',
+        username: 'user',
+      };
+
+      try {
+        await signUp(input);
+      } catch (error) {
+        return done();
+      }
+
+      throw new Error('Test failed');
+    });
+
+    it('successfully creates a user given a valid input', async () => {
+      const input: ISignUpInput = {
+        confirmPassword: '1234567890',
+        password: '1234567890',
+        email: 'user@test.com',
+        username: 'newuser',
+      };
+
+      const user = await signUp(input);
+
+      expect(user.username).toEqual(input.username);
+      expect(user.email).toEqual(input.email);
+    });
   });
 });
--- src/api/users/user.repository.ts
+++ src/api/users/user.repository.ts
@@ -1,3 +1,35 @@
+import { InvalidUserInputError } from '../../errors/invalid-user-input.error';
+import { UserDocument, UserModel } from './database/user.entity';
 import { ISignUpInput } from './interfaces/sign-up-input.interface';
 
-const signUp = (input: ISignUpInput) => {};
+const signUp = async (input: ISignUpInput): Promise<any> => {
+  const { confirmPassword, email, password, username } = input;
+
+  const checkEmail = await UserModel.findOne({ email });
+
+  if (checkEmail) {
+    throw new InvalidUserInputError();
+  }
+
+  const checkUserName = await UserModel.findOne({ username });
+
+  if (checkUserName) {
+    throw new InvalidUserInputError();
+  }
+
+  if (password !== confirmPassword) {
+    throw new InvalidUserInputError();
+  }
+
+  const newUser = UserModel.build({
+    email,
+    password,
+    username,
+  });
+
+  await newUser.save();
+
+  return newUser;
+};
+
+export { signUp };
