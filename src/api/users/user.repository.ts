96b1f80c08cef6e22c3ecc75987522737ed05df2
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-14 16:21:50 -0400

    refactored invalid user input error and signup function

--- src/api/users/user.repository.ts
+++ src/api/users/user.repository.ts
@@ -1,24 +1,39 @@
+import { IErrorPayload } from '../../common/interfaces/error-payload.interface';
 import { InvalidUserInputError } from '../../errors/invalid-user-input.error';
 import { UserDocument, UserModel } from './database/user.entity';
 import { ISignUpInput } from './interfaces/sign-up-input.interface';
 
-const signUp = async (input: ISignUpInput): Promise<any> => {
+const signUp = async (input: ISignUpInput): Promise<UserDocument> => {
   const { confirmPassword, email, password, username } = input;
+
+  const errors: IErrorPayload[] = [];
 
   const checkEmail = await UserModel.findOne({ email });
 
   if (checkEmail) {
-    throw new InvalidUserInputError();
+    errors.push({
+      message: 'email already in use',
+      field: 'email',
+    });
   }
 
   const checkUserName = await UserModel.findOne({ username });
 
   if (checkUserName) {
-    throw new InvalidUserInputError();
+    errors.push({
+      message: 'username already in use',
+      field: 'username',
+    });
   }
 
   if (password !== confirmPassword) {
-    throw new InvalidUserInputError();
+    errors.push({
+      message: 'passwords do not match',
+    });
+  }
+
+  if (errors.length > 0) {
+    throw new InvalidUserInputError(errors);
   }
 
   const newUser = UserModel.build({
--- src/errors/invalid-user-input.error.ts
+++ src/errors/invalid-user-input.error.ts
@@ -4,13 +4,17 @@ import { IErrorPayload } from '../common/interfaces/error-payload.interface';
 export class InvalidUserInputError extends BaseError {
   statusCode = 400;
 
-  constructor() {
+  private messages: IErrorPayload[];
+
+  constructor(messages: IErrorPayload[] = []) {
     super('Invalid user input');
 
     Object.setPrototypeOf(this, InvalidUserInputError.prototype);
+
+    this.messages = messages;
   }
 
   serializeErrors(): IErrorPayload[] {
-    return [];
+    return this.messages;
   }
 }
