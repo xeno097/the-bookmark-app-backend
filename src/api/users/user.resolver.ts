dbd4ce5e3e6ce40dcb4d29526da528a3a3fc5e0d
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-14 20:23:20 -0400

    implemented and added signUp mutation to apolloServer

--- src/api/users/user.resolver.ts
+++ src/api/users/user.resolver.ts
@@ -1,1 +1,27 @@
-const signUp = () => {};
+import { GqlCustomExecutionContext } from '../../common/interfaces/graphql-custom-context.interface';
+import { ISignUpInput } from './interfaces/sign-up-input.interface';
+import { signUp as signUpRepo } from './user.repository';
+import { generateToken } from './utils/jwt.utils';
+
+const signUp = async (
+  parent: any,
+  args: { input: ISignUpInput },
+  context: GqlCustomExecutionContext,
+  info: any,
+) => {
+  const { input } = args;
+  const { res } = context;
+
+  const user = await signUpRepo(input);
+
+  const jwt = generateToken({ id: user.id, username: user.username });
+
+  res.cookie('authentication', jwt);
+
+  return {
+    jwt,
+    user,
+  };
+};
+
+export { signUp };
