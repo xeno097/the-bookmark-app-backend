2014bd07c14465f674d3f905e8f2ef7df34e894c
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-14 22:09:51 -0400

    added cookie configuration to signup mutation

--- src/api/users/user.resolver.ts
+++ src/api/users/user.resolver.ts
@@ -1,14 +1,15 @@
 import { GqlCustomExecutionContext } from '../../common/interfaces/graphql-custom-context.interface';
+import { IAuthPayload } from './interfaces/auth-payload.interface';
 import { ISignUpInput } from './interfaces/sign-up-input.interface';
 import { signUp as signUpRepo } from './user.repository';
-import { generateToken } from './utils/jwt.utils';
+import { AUTH_EXPIRATION_TIME, generateToken } from './utils/jwt.utils';
 
 const signUp = async (
   parent: any,
   args: { input: ISignUpInput },
   context: GqlCustomExecutionContext,
   info: any,
-) => {
+): Promise<IAuthPayload> => {
   const { input } = args;
   const { res } = context;
 
@@ -16,7 +17,15 @@ const signUp = async (
 
   const jwt = generateToken({ id: user.id, username: user.username });
 
-  res.cookie('authentication', jwt);
+  const date = new Date();
+
+  const expiresIn = date.setSeconds(date.getSeconds() + AUTH_EXPIRATION_TIME);
+
+  res.cookie('authentication', jwt, {
+    httpOnly: true,
+    secure: false,
+    expires: new Date(expiresIn),
+  });
 
   return {
     jwt,
--- src/api/users/utils/jwt.utils.ts
+++ src/api/users/utils/jwt.utils.ts
@@ -1,8 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { appConfig } from '../../../config/app.config';
 
+export const AUTH_EXPIRATION_TIME = 60 * 60;
+
 export const generateToken = (userData: { username: string; id: string }) => {
-  const token = jwt.sign(userData, appConfig.jwtSecret);
+  const token = jwt.sign(userData, appConfig.jwtSecret, {
+    expiresIn: AUTH_EXPIRATION_TIME,
+  });
 
   return token;
 };
