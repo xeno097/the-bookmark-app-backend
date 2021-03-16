28e3d37a1fb1f70dbc0fd111d9c00c677d93f281
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-16 17:38:11 -0400

    defined signIn mutation interface

--- src/api/users/interfaces/user-resolver.interface.ts
+++ src/api/users/interfaces/user-resolver.interface.ts
@@ -1,11 +1,18 @@
 import { GqlCustomExecutionContext } from '../../../common/interfaces/graphql-custom-context.interface';
-import { UserDocument } from '../database/user.entity';
+import { IAuthPayload } from './auth-payload.interface';
+import { ISignInInput } from './sign-in-input.interface';
 import { ISignUpInput } from './sign-up-input.interface';
 
 export interface IUserMutations {
   signUp: (
     parent: any,
     args: { input: ISignUpInput },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<IAuthPayload>;
+  signIn: (
+    parent: any,
+    args: { input: ISignInInput },
     context: GqlCustomExecutionContext,
     info: any,
   ) => Promise<any>;
