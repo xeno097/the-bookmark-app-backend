ce48f56f9082cd06652bb2a2673efa72bd4630fb
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-16 20:22:22 -0400

    added logic to extract user data from jwt in gqlContext function

--- src/apollo/utils/gql-context.util.ts
+++ src/apollo/utils/gql-context.util.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
+import { AUTH_PROPERTY_KEY } from '../../common/constants';
 import { GqlCustomExecutionContext } from '../../common/interfaces/graphql-custom-context.interface';
+import { IJwtPayload } from '../../common/interfaces/jwt-payload.interface';
+import jwt from 'jsonwebtoken';
+import { appConfig } from '../../config/app.config';
 
 export const gqlContext = (ctx: {
   req: Request;
@@ -7,9 +11,20 @@ export const gqlContext = (ctx: {
 }): GqlCustomExecutionContext => {
   const { req, res } = ctx;
 
+  const jwtCookie = req.cookies[AUTH_PROPERTY_KEY];
+  const jwtHeader = req.headers.authorization?.split(' ')[1];
+
+  const token = jwtCookie || jwtHeader;
+
+  let user = undefined;
+
+  try {
+    user = jwt.verify(token, appConfig.jwtSecret) as IJwtPayload;
+  } catch (error) {}
+
   return {
     req,
     res,
-    user: undefined,
+    user,
   };
 };
