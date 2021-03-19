664803839c6a3469d07815aed8962ae8b38e6007
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-18 21:21:15 -0400

    implemented tests for getOneBookmark function

--- src/api/bookmarks/__tests__/bookmark.repository.test.ts
+++ src/api/bookmarks/__tests__/bookmark.repository.test.ts
@@ -1,5 +1,89 @@
+import { BookmarkModel } from '../database/bookmark.entity';
+import mongoose from 'mongoose';
+import { IGetOneBookmark } from '../interfaces/get-one-bookmark-input.interface';
+import { getOneBookmark } from '../bookmark.repository';
+
 describe('BookmarkRepository', () => {
-  describe('getOneBookmark', () => {});
+  const createBookmarkSetup = async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+
+    const bookmark = BookmarkModel.build({
+      name: 'test bookmark',
+      tags: [],
+      userId,
+      description: 'a random description',
+    });
+
+    await bookmark.save();
+
+    return bookmark;
+  };
+
+  afterAll(async () => {
+    await BookmarkModel.deleteMany({});
+  });
+
+  describe('getOneBookmark', () => {
+    it('throws an error if given an id it cannot find a bookmark', async (done) => {
+      const id = mongoose.Types.ObjectId().toHexString();
+
+      const input: IGetOneBookmark = {
+        id,
+      };
+
+      try {
+        await getOneBookmark(input);
+      } catch (error) {
+        return done();
+      }
+
+      throw new Error('Test failed');
+    });
+
+    it('throws an error if given a bookmark and a user id it cannot find a bookmark', async (done) => {
+      const id = mongoose.Types.ObjectId().toHexString();
+      const userId = mongoose.Types.ObjectId().toHexString();
+
+      const input: IGetOneBookmark = {
+        id,
+        userId,
+      };
+
+      try {
+        await getOneBookmark(input);
+      } catch (error) {
+        return done();
+      }
+
+      throw new Error('Test failed');
+    });
+
+    it('successfully returns a bookmark given a valid id', async () => {
+      const testBookmark = await createBookmarkSetup();
+
+      const input: IGetOneBookmark = {
+        id: testBookmark.id,
+      };
+
+      const bookmark = await await getOneBookmark(input);
+
+      expect(bookmark.id).toEqual(input.id);
+    });
+
+    it('successfully return a bookmark given a combination of a valid bookmark and user id', async () => {
+      const testBookmark = await createBookmarkSetup();
+
+      const input: IGetOneBookmark = {
+        id: testBookmark.id,
+        userId: testBookmark.userId,
+      };
+
+      const bookmark = await await getOneBookmark(input);
+
+      expect(bookmark.id).toEqual(input.id);
+      expect(bookmark.userId).toEqual(input.userId);
+    });
+  });
 
   describe('getAllBookmarks', () => {});
 
