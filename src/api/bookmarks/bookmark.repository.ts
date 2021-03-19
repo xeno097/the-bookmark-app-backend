331b38eb5511b625fb274a0f5d635f3a34acf07c
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-18 22:54:51 -0400

    implemented and tested getAllBookmarks

--- src/api/bookmarks/bookmark.repository.ts
+++ src/api/bookmarks/bookmark.repository.ts
@@ -1,5 +1,6 @@
 import { NotFoundError } from '../../errors/not-found.error';
 import { BookmarkDocument, BookmarkModel } from './database/bookmark.entity';
+import { IFilterBookmarks } from './interfaces/filter-bookmarks-input.interface';
 import { IGetOneBookmark } from './interfaces/get-one-bookmark-input.interface';
 
 const getOneBookmark = async (
@@ -26,7 +27,35 @@ const getOneBookmark = async (
   return bookmark;
 };
 
-const deleteBookmark = async (input: IGetOneBookmark): Promise<any> => {
+const getAllBookmarks = async (
+  input: IFilterBookmarks = {},
+): Promise<any[]> => {
+  const { filter, limit, start } = input;
+
+  const bookmarks = BookmarkModel.find();
+
+  if (start) {
+    bookmarks.skip(start);
+  }
+
+  if (limit) {
+    bookmarks.limit(limit);
+  }
+
+  if (filter && filter.userId) {
+    const { userId } = filter;
+
+    bookmarks.where({ userId });
+  }
+
+  await bookmarks.exec();
+
+  return bookmarks;
+};
+
+const deleteBookmark = async (
+  input: IGetOneBookmark,
+): Promise<BookmarkDocument> => {
   const bookmark = await getOneBookmark(input);
 
   await BookmarkModel.findByIdAndDelete(bookmark.id);
@@ -34,4 +63,4 @@ const deleteBookmark = async (input: IGetOneBookmark): Promise<any> => {
   return bookmark;
 };
 
-export { getOneBookmark, deleteBookmark };
+export { getOneBookmark, deleteBookmark, getAllBookmarks };
