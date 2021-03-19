60cd481e412cf537ab1ddf2ac133e0d2d97bc6c0
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-18 21:21:36 -0400

    implemented and tested getOneBookmark function

--- src/api/bookmarks/bookmark.repository.ts
+++ src/api/bookmarks/bookmark.repository.ts
@@ -1,3 +1,27 @@
-const getOneBookmark = () => {};
+import { NotFoundError } from '../../errors/not-found.error';
+import { BookmarkModel } from './database/bookmark.entity';
+import { IGetOneBookmark } from './interfaces/get-one-bookmark-input.interface';
+
+const getOneBookmark = async (input: IGetOneBookmark): Promise<any> => {
+  const { id, userId } = input;
+
+  let filter = {};
+
+  if (id) {
+    filter = { _id: id };
+  }
+
+  if (userId) {
+    filter = { ...filter, userId };
+  }
+
+  const bookmark = await BookmarkModel.findOne(filter);
+
+  if (!bookmark) {
+    throw new NotFoundError();
+  }
+
+  return bookmark;
+};
 
 export { getOneBookmark };
