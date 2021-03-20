3c67731f874ff7b70d86953f21dea443861b3f41
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-20 17:09:42 -0400

    defined bookmarks query tests

--- src/api/bookmarks/__tests__/bookmark.resolver.test.ts
+++ src/api/bookmarks/__tests__/bookmark.resolver.test.ts
@@ -11,6 +11,7 @@ import { ISignInInput } from '../../users/interfaces/sign-in-input.interface';
 import { TagModel } from '../../tags/database/tag.entity';
 import { generateSlug } from '../../../common/functions/generate-slug';
 import crypto from 'crypto';
+import { IFilterBookmarks } from '../interfaces/filter-bookmarks-input.interface';
 
 describe('BookmarkResolver', () => {
   afterEach(async () => {
@@ -86,6 +87,22 @@ describe('BookmarkResolver', () => {
             }
             url
         }
+      }
+    `;
+
+  const BOOKMARKS_QUERY = `
+      query($input: FilterBookmarksInput){
+        bookmarks(input:$input){
+            id
+            name
+            description
+            tags{
+                id
+                name
+                slug
+            }
+            url
+          }
       }
     `;
 
@@ -188,6 +205,144 @@ describe('BookmarkResolver', () => {
         bookmark.description,
       );
       expect(loggedRes.body.data.bookmark.url).toEqual(bookmark.url);
+    });
+  });
+
+  describe('bookmarks', () => {
+    it('throws an error if the user is not logged in', async () => {
+      const input: IFilterBookmarks = {};
+
+      const res = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .send({ query: BOOKMARKS_QUERY, variables: { input } });
+
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.data).toBeNull();
+    });
+
+    it("returns an empty array if there aren't bookmarks that belong to the user in the db", async () => {
+      const newUser = await signUpUserSetup();
+
+      const loginInput: ISignInInput = {
+        password: '1234567890',
+        username: newUser.username,
+      };
+
+      const res = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: SIGN_IN_MUTATION,
+          variables: { input: loginInput },
+        });
+
+      expect(res.body.errors).toBeUndefined();
+      expect(res.body.data).toBeDefined();
+      expect(res.body.data.signIn.jwt).toBeDefined();
+      expect(res.body.data.signIn.user.email).toEqual(newUser.email);
+      expect(res.body.data.signIn.user.username).toEqual(newUser.username);
+      expect(res.body.data.signIn.user.email).toEqual(newUser.email);
+
+      const jwtToken = res.body.data.signIn.jwt;
+
+      const input: IFilterBookmarks = {};
+
+      const loggedRes = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .set('Cookie', [`${AUTH_PROPERTY_KEY}=${jwtToken}`])
+        .send({
+          query: BOOKMARKS_QUERY,
+          variables: { input },
+        });
+
+      expect(loggedRes.body.errors).toBeUndefined();
+      expect(loggedRes.body.data).toBeDefined();
+    });
+
+    it('returns all the bookmarks that belong to the user', async () => {
+      const newUser = await signUpUserSetup();
+      const bookmark1 = await createBookmarkSetup(newUser.id);
+      const bookmark2 = await createBookmarkSetup(newUser.id);
+
+      const loginInput: ISignInInput = {
+        password: '1234567890',
+        username: newUser.username,
+      };
+
+      const res = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: SIGN_IN_MUTATION,
+          variables: { input: loginInput },
+        });
+
+      expect(res.body.errors).toBeUndefined();
+      expect(res.body.data).toBeDefined();
+      expect(res.body.data.signIn.jwt).toBeDefined();
+      expect(res.body.data.signIn.user.email).toEqual(newUser.email);
+      expect(res.body.data.signIn.user.username).toEqual(newUser.username);
+      expect(res.body.data.signIn.user.email).toEqual(newUser.email);
+
+      const jwtToken = res.body.data.signIn.jwt;
+
+      const input: IFilterBookmarks = {};
+
+      const loggedRes = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .set('Cookie', [`${AUTH_PROPERTY_KEY}=${jwtToken}`])
+        .send({
+          query: BOOKMARKS_QUERY,
+          variables: { input },
+        });
+
+      expect(loggedRes.body.errors).toBeUndefined();
+      expect(loggedRes.body.data).toBeDefined();
+      expect(loggedRes.body.data.bookmarks.length).toEqual(2);
+      expect(loggedRes.body.data.bookmarks[0].id).toEqual(bookmark1.id);
+      expect(loggedRes.body.data.bookmarks[1].id).toEqual(bookmark2.id);
+    });
+
+    it('returns an array of one bookmark that belongs to the user', async () => {
+      const newUser = await signUpUserSetup();
+      const bookmark1 = await createBookmarkSetup(newUser.id);
+      await createBookmarkSetup(newUser.id);
+
+      const loginInput: ISignInInput = {
+        password: '1234567890',
+        username: newUser.username,
+      };
+
+      const res = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: SIGN_IN_MUTATION,
+          variables: { input: loginInput },
+        });
+
+      expect(res.body.errors).toBeUndefined();
+      expect(res.body.data).toBeDefined();
+      expect(res.body.data.signIn.jwt).toBeDefined();
+      expect(res.body.data.signIn.user.email).toEqual(newUser.email);
+      expect(res.body.data.signIn.user.username).toEqual(newUser.username);
+      expect(res.body.data.signIn.user.email).toEqual(newUser.email);
+
+      const jwtToken = res.body.data.signIn.jwt;
+
+      const input: IFilterBookmarks = {
+        limit: 1,
+      };
+
+      const loggedRes = await request(app)
+        .post(GRAPHQL_ENDPOINT)
+        .set('Cookie', [`${AUTH_PROPERTY_KEY}=${jwtToken}`])
+        .send({
+          query: BOOKMARKS_QUERY,
+          variables: { input },
+        });
+
+      expect(loggedRes.body.errors).toBeUndefined();
+      expect(loggedRes.body.data).toBeDefined();
+      expect(loggedRes.body.data.bookmarks.length).toEqual(1);
+      expect(loggedRes.body.data.bookmarks[0].id).toEqual(bookmark1.id);
     });
   });
 });
