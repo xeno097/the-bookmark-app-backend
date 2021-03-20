cae4acd8841d3c8803199f1692b28d4287d94523
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-20 17:10:39 -0400

    implemented bookmarks query

--- src/api/bookmarks/bookmark.resolver.ts
+++ src/api/bookmarks/bookmark.resolver.ts
@@ -1,7 +1,9 @@
 import { authorizeUser } from '../../common/functions/authorize-user';
 import { GqlCustomExecutionContext } from '../../common/interfaces/graphql-custom-context.interface';
-import { getOneBookmark } from './bookmark.repository';
+import { getAllBookmarks, getOneBookmark } from './bookmark.repository';
+import { BookmarkDocument } from './database/bookmark.entity';
 import { IBookmarkQueries } from './interfaces/bookmark-resolver.interface';
+import { IFilterBookmarks } from './interfaces/filter-bookmarks-input.interface';
 import { IGetOneBookmark } from './interfaces/get-one-bookmark-input.interface';
 
 const bookmark = async (
@@ -21,11 +23,24 @@ const bookmark = async (
 
 const bookmarks = async (
   parent: any,
-  args: any,
+  args: { input: IFilterBookmarks },
   context: GqlCustomExecutionContext,
   info: any,
-): Promise<any[]> => {
-  return [];
+): Promise<BookmarkDocument[]> => {
+  const { user } = context;
+  const validatedUser = authorizeUser(user);
+  const { input } = args;
+
+  const internalInput: IFilterBookmarks = {
+    ...input,
+    filter: {
+      userId: validatedUser.id,
+    },
+  };
+
+  const bookmarks = await getAllBookmarks(internalInput);
+
+  return bookmarks;
 };
 
 const bookmarkQueries: IBookmarkQueries = {
