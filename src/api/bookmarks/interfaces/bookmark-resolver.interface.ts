585c6299b5586772bf397adf5b46241c609fd243
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-03-20 13:59:13 -0400

    defined bookmark resolver interface

---
+++ src/api/bookmarks/interfaces/bookmark-resolver.interface.ts
@@ -0,0 +1,40 @@
+import { GqlCustomExecutionContext } from '../../../common/interfaces/graphql-custom-context.interface';
+import { ICreateBookmarkInput } from './create-bookmark-input.interface';
+import { IGetOneBookmark } from './get-one-bookmark-input.interface';
+import { IUpdateBookmarkInput } from './update-bookmark-input.interface';
+
+export interface BookmarkQueries {
+  bookmark: (
+    parent: any,
+    args: { input: IGetOneBookmark },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<any>;
+  bookmarks: (
+    parent: any,
+    args: any,
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<any[]>;
+}
+
+export interface BookmarkMutations {
+  createBookmark: (
+    parent: any,
+    args: { input: ICreateBookmarkInput },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<any>;
+  updateBookmark: (
+    parent: any,
+    args: { input: IUpdateBookmarkInput },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<any>;
+  deleteBookmark: (
+    parent: any,
+    args: { input: IGetOneBookmark },
+    context: GqlCustomExecutionContext,
+    info: any,
+  ) => Promise<any>;
+}
